593356c0c37c257b56d108fdd2d749e6cd620ce3
Srilakshmiyendra <sriyendra1998@gmail.com>
2024-10-22 15:06:49 +0530

    Delete authproject directory

--- authproject/authapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Authenication,SupervisorAuthenication
-
-# Register your models here.
-admin.site.register(Authenication)
-admin.site.register(SupervisorAuthenication)
